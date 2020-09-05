6c7906098e77d90cbe661bc65c41dab60580c84c
Frank Mwangi <frankmwangi007@gmail.com>
2020-09-05 11:41:34 +0300

    Introduction to layout files

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,17 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-     integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <title>Todos</title>
-</head>
-<body>
-    
+@extends('layouts.app')
 
-  <div class="container">
-    <h1 class="text-center my-5">TODOS PAGE</h1>
+@section('title')
+    Todos List
+@endsection
+
+
+@section('content')
+<h1 class="text-center my-5">TODOS PAGE</h1>
        <div class="row justify-content-center">
         <div class="col-md-8 ">
         <div class="card card-default">
@@ -34,7 +29,4 @@
         </div>
         </div>
        </div>
-  </div>
-
-</body>
-</html>
+@endsection
--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -1,15 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-     integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <title>Todo item</title>
-</head>
-<body>
-    <div class="container">
-        <h1 class="text-center my-5">
+@extends('layouts.app')
+
+
+
+@section('title')
+    single Todo: {{ $todo->name}}
+@endsection
+
+
+@section('content')
+<h1 class="text-center my-5">
             {{ $todo->name}}
         </h1>
         <div class="card card-default">
@@ -24,6 +23,4 @@
           </div>
         </div>    
         </div>
-    </div>
-</body>
-</html>
+@endsection
