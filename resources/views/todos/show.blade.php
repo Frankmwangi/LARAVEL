5e2c1dac676e544eabfc75a091802da865410d62
Frank Mwangi <frankmwangi007@gmail.com>
2020-09-07 11:00:37 +0300

    Editing and deleting todos

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -11,16 +11,25 @@
 <h1 class="text-center my-5">
             {{ $todo->name}}
         </h1>
+        <div class="row justify-content-center">
+                <div class="col-md-6">
         <div class="card card-default">
             <div class="card-header">
                 Details
             </div>
         <div class="card-body">
-          <div class="row justify-content-center">
-                <div class="col-md-6">
-                    {{$todo->description}}
-                </div>
-          </div>
+            {{$todo->description}}
+        </div>
+               
+         
+        
+        </div>
+        <a href="/todos/{{$todo->id}}/edit  "class="btn btn-info btn-sm my-2">
+                    Edit
+                </a>
+                <a href="/todos/{{$todo->id}}/delete" class="btn btn-danger  my-2">
+                    Delete
+                </a>
         </div>    
         </div>
 @endsection
